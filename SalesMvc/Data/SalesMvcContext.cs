cd6f58db1af87cd2492735d57a41e81a271c7eb9
Henrique Kiyomi <henriquekiyomi12@gmail.com>
2023-06-19 14:57:50 -0300

    Other Entities and migration

--- SalesMvc/Data/SalesMvcContext.cs
+++ SalesMvc/Data/SalesMvcContext.cs
@@ -14,6 +14,8 @@ namespace SalesMvc.Data
         {
         }
 
-        public DbSet<SalesMvc.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
