6885bc9f7021e32c062d736c03714f69e5c62b60
Henrique Kiyomi <henriquekiyomi12@gmail.com>
2023-06-25 22:22:16 -0300

    Simple Create Form

--- SalesMvc/Controllers/SellersController.cs
+++ SalesMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesMvc.Models;
 using SalesMvc.Services;
 
 namespace SalesMvc.Controllers
@@ -16,6 +17,18 @@ namespace SalesMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
